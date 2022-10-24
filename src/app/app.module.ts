977b862580274dc9dd657836e121a2b94be06ca2
me-dira <mediraworkm@gmail.com>
2022-10-24 23:13:02 +0330

    feat: added shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppLoggerLevel, AppProviders } from 'src/const';
 import { MainLoggerService } from 'src/utilities';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, SharedModule],
   providers: [
     MainLoggerService,
     { provide: AppProviders.LoggerLevel, useValue: AppLoggerLevel.DEVELOPMENT },
